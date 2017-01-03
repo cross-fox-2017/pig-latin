a440717e44cfedb5621a3ee465846e260283339e
Dale <dellryuzi@gmail.com>
2017-01-03 14:36:00 +0700

    baca rl blom bisa

--- pig_latin.js
+++ pig_latin.js
@@ -1,24 +1,24 @@
-"use strict"
 
 //use readline to fix this challenge
 const readline = require('readline');
+const prompt = require('prompt');
 
 let rl = readline.createInterface({
   input:process.stdin,
   output:process.stdout
 });
 
-rl.pigLatin('What do you think of Node.js? ', (word) => {
 
-      var array = word.split(" ");
+rl.prompt();
+//rl.setPrompt(prompt,length)
+rl.on('PigLatin ', (word) => {
+
+  var pigLatin = function(word) {
+  //    var array = word.toString();
       var vowels = /[aieou]/gi;
       var result = '';
-      var sentence ="";
-      // console.log(array);
-      // console.log(word);
 
-      for(j=0; j< array.length-1;j++){
-          for(var i = 0; i < array.length[j]; i++) { //find vowel in word
+          for(var i = 0; i < word.length-1; i++) { //find vowel in word
               if(word[i] === 'a' ||
                 word[i] === 'e' ||
                 word[i] === 'i' ||
@@ -26,20 +26,23 @@ rl.pigLatin('What do you think of Node.js? ', (word) => {
                 word[i] === 'o'
 
             ) {
-                  for(var x = i; x < array.length[j]; x++){ //mengambil dri awal smpai
+                  for(var x = i; x < word.length; x++){ //mengambil dri awal smpai
                       result += word[x];
                   }
                   for(var n = 0; n < i; n++){
                       result += word[n];
                   }
-                 result + "ay";
+                  return result + "ay";
               }
           }
-          return sentence += result ;
-        }
+  }
+
+  console.log(result);
 
 
-//  console.log(pigLatin("duck happy"));
+  }
+
+
 
   rl.close();
 
