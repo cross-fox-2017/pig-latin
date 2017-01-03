f8d12400378b06678ce7641ee3968803a2f732dd
Ida Bagus Chahya Dhegana & yoma sofwan <pair+dgana.yomaswn@hacktiv8.com>
2017-01-03 19:47:09 +0700

    all release

--- pig_latin.js
+++ pig_latin.js
@@ -1,4 +1,62 @@
 "use strict"
 
-//use readline to fix this challenge
+const convertSingleWord = (str) => {
+  let newArr = [];
+  let newArr2 = [];
+  let vowel = ['a','i','u','e','o'];
+  let limit = 0;
+
+  str = str.split('');
+
+  for(let i = 0; i < vowel.length; i++){
+    if(str[0] === vowel[i]){
+      return str.join('');
+    }
+  }
+
+  for(let i = 0; i < vowel.length; i++){
+    let idx = str.indexOf(vowel[i])
+    if(idx > 0){
+      limit = idx;
+      break;
+    }
+  }
+
+  for(let i = 0; i < limit; i++){
+    newArr.push(str[i]);
+  }
+
+  for(let i = limit; i < str.length; i++){
+    newArr2.push(str[i]);
+  }
+
+  return newArr2.join('')+newArr.join('')+'ay.';
+}
+
+function convertCompleteSentence(str){
+  str = str.split(' ');
+  let newStr = '';
+
+  for(let i = 0; i < str.length; i++){
+    newStr += convertSingleWord(str[i]) + ' ';
+  }
+  return newStr;
+}
+
 const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.setPrompt('Pig Latin > ');
+rl.prompt();
+
+rl.on('line', (answer) => {
+  // TODO: Log the answer in a database
+  console.log(convertCompleteSentence(answer));
+  rl.prompt();
+}).on('close', function(){
+  console.log('out');
+  rl.close();
+})
