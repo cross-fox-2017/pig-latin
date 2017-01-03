1bc54c34015438636a5279e7714f2a3c3556e8dd
Syanmil <syanmil@yahoo.com>
2017-01-03 10:30:44 +0700

    initialize readline but the close still didn't work

--- pig_latin.js
+++ pig_latin.js
@@ -2,3 +2,18 @@
 
 //use readline to fix this challenge
 const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.setPrompt(`Silahkan masukkan kata atau kalimat anda disini \n`)
+rl.prompt()
+rl.on('line', (word) => {
+    piglatin(word);
+})
+rl.close
+
+function piglatin (kata){
+  rl.close
+  return kata
+}
