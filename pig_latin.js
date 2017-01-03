dda57d07c1bfe77731503630f56149aaa6df4dee
Raditya <arya.raditya26@gmail.com>
2017-01-03 21:43:19 +0700

    Selesai (final)

--- pig_latin.js
+++ pig_latin.js
@@ -9,32 +9,28 @@ const rl = readline.createInterface({
 });
 
 rl.question('input : ', (input) => {
-
-
   //code starts here...
-
-
     let sentenceArr = input.split(' ');
     let holdArr = [];
     let answerStr = '';
     let vowel = ['A', 'I', 'U', 'E', 'O', 'a', 'i', 'u', 'e', 'o'];
 
-  function pig_latin (answer) {
+  function pig_latin (sentenceArr) {
 
-    if (vowel.indexOf(answer[0]) >= 0) {
-      return answer;
+    if (vowel.indexOf(sentenceArr[0]) >= 0) {
+      return sentenceArr;
       }
       else {
           // vowel pertama ada di index berapa
-          let cekIndex = ''
-          for (var i = 0; i < answer.length; i++) {
-            if (vowel.indexOf(answer[i]) >= 0 ){
+          let cekIndex = '-1'
+          for (var i = 0; i < sentenceArr.length; i++) {
+            if (vowel.indexOf(sentenceArr[i]) >= 0 ){
                 // vowel pertama ada di index ke i
                 cekIndex = i;
                 // potong konsonan di awal
-                answerStr += answer.substring(0, i);
+                answerStr = sentenceArr.substring(0, i);
                 // potong sisa + potongan konsonan + 'ay'
-                return answer.slice(cekIndex, answer.length) + answerStr + 'ay';
+                return sentenceArr.slice(cekIndex, sentenceArr.length) + answerStr + 'ay';
             }
           }
         }
