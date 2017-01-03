5372520e8397f7981749c963fab314c701d42eb8
Your Name <name@example.com>
2017-01-03 10:41:05 +0700

    Complete Converted Pig Latin

--- pig_latin.js
+++ pig_latin.js
@@ -1,4 +1,37 @@
 "use strict"
 
-//use readline to fix this challenge
 const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output:process.stdout
+});
+
+
+function pig_latin(input){
+    var vowel = ['a','i','u','e','o']
+    var firstword = input.slice(0,1);
+    var lastword = input.slice(1,input.length)
+    if(vowel.indexOf(firstword)!= -1){
+      return input
+    }else{
+      return lastword+firstword+"ay"
+    }
+}
+
+function pig_latin_sentences(input){
+  var hasil = ""
+  var tampung = input.split(" ")
+  for (var i = 0; i < tampung.length; i++) {
+    hasil += pig_latin(tampung[i])+" "
+  }
+  return hasil
+}
+
+rl.setPrompt('Masukkan kata / Kalimat : \n');
+rl.prompt();
+rl.on('line', (input) =>{
+  console.log(`Your Input "${input}" Converted Pig Latin`);
+  console.log(pig_latin_sentences(input));
+  console.log(`ReInput or CTRL+C to exit`);
+})
