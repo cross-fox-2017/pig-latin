90520e3a1146d5e5e25ebbf6a7f8ae21a673eca0
Dale <dellryuzi@gmail.com>
2017-01-03 20:07:25 +0700

    readline re-ask

--- pig_latin.js
+++ pig_latin.js
@@ -8,41 +8,53 @@ let rl = readline.createInterface({
   output:process.stdout
 });
 
-rl.question('konversi : ', (wordx) => {
+rl.prompt();
+// rl.question('konversi : ', (wordx) => {
+//
+// }) ;
 
-    let array = wordx.split(" ");
-    let temp = "";
-    for(let i = 0 ; i<array.length ;i++){
-      temp += " "+ pig_latin(array[i]);
-    //console.log(temp);
-    }
+rl.on('line', (line) => {
+
+      let array = line.split(" ");
+      let temp = "";
+      for(let i = 0 ; i<array.length ;i++){
+        temp += " "+ pig_latin(array[i]);
+      //console.log(temp);
+      }
 
 
-    function pig_latin(word){
-      var vowels = /[aieou]/gi;
-      var result = '';
+      function pig_latin(word){
+        var vowels = /[aieou]/gi;
+        var result = '';
+        var vowelsx = ['a','i','u','e','o'];
+        //console.log(word[0]);
 
-          for(var i = 0; i < word.length-1; i++) { //find vowel in word
-              if(word[i] === 'a' ||
-                word[i] === 'e' ||
-                word[i] === 'i' ||
-                word[i] === 'u' ||
-                word[i] === 'o') {
-                  for(var x = i; x < word.length; x++){ //mengambil dri awal smpai
-                      result += word[x];
-                  }
-                  for(var n = 0; n < i; n++){
-                      result += word[n];
-                  }
-                   result += "ay";
-              }
-          }
+        if(vowelsx.indexOf(word[0]) === -1  ){
+            for(var i = 0; i < word.length-1; i++) { //find vowel in word
 
-          //console.log(result);
-          return result;
+                if(word[i] === 'a' ||
+                  word[i] === 'e' ||
+                  word[i] === 'i' ||
+                  word[i] === 'u' ||
+                  word[i] === 'o') {
+                    for(var x = i; x < word.length; x++){ //mengambil dri awal smpai
+                        result += word[x];
+                    }
+                    for(var n = 0; n < i; n++){
+                        result += word[n];
+                    }
+                     result += "ay";
+                }
+            }
 
-  rl.close();
-}
 
-console.log(temp);
+            //console.log(result);
+          } else { result += word;}
+            return result;
+
+
+    //rl.close();
+  }
+
+  console.log(temp);
 });
